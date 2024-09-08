102ad6cfc31486570ac61a73c89634e04837428f
Albert <39496218+albertturon1@users.noreply.github.com>
2024-09-08 12:18:37 +0200

    feat: create useWindowDimensions and replace window.height in proper places

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -3,15 +3,17 @@ import type { FilePickerRow } from "@/components/file-picker";
 import { VirtualizedTable } from "@/components/virtualized-table/virtualized-table";
 import { Landing } from "@/features/home/components/landing";
 import { NAVBAR_HEIGHT } from "@/routes/__root";
+import { useWindowDimensions } from "@/lib/useWindowDimensions";
 
 export function HomePage() {
 	const [data, setData] = useState<FilePickerRow[]>([]);
+	const { height } = useWindowDimensions();
 
 	return (
 		<div
 			className="overflow-hidden flex flex-col gap-y-10"
 			style={{
-				height: window.innerHeight - NAVBAR_HEIGHT,
+				height: height - NAVBAR_HEIGHT,
 			}}
 		>
 			{data.length === 0 ? (
