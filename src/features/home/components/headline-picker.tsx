fd2fcd97ac33d268d338f714b638b1e24bf60b50
Albert <39496218+albertturon1@users.noreply.github.com>
2024-09-19 21:54:33 +0200

    fix: change headline-picker overlay color for dark mode

--- src/features/home/components/headline-picker.tsx
+++ src/features/home/components/headline-picker.tsx
@@ -1,12 +1,24 @@
 import type { FilePickerProps } from "@/components/file-picker";
 import { FilePicker } from "@/components/file-picker";
 import { Logo } from "@/components/logo";
+import { cn } from "@/lib/utils";
+import { getValueFromSystemTheme, useTheme } from "@/components/theme-provider";
 
 export type HeadlineWithPickerProps = Pick<FilePickerProps, "onFileChange">;
 
 export function HeadlineWithPicker({ onFileChange }: HeadlineWithPickerProps) {
+	const { theme } = useTheme();
+	const currentTheme = theme === "system" ? getValueFromSystemTheme() : theme;
+	const overlayColor =
+		currentTheme === "light" ? "bg-slate-100/10" : "bg-black/60";
+
 	return (
-		<div className="absolute z-10 top-0 right-0 left-0 bottom-0 flex flex-1 flex-col bg-slate-100/10">
+		<div
+			className={cn(
+				"absolute z-10 top-0 right-0 left-0 bottom-0 flex flex-1 flex-col",
+				overlayColor,
+			)}
+		>
 			<div className="flex flex-1 bg-background opacity-50" />
 			<div className="absolute z-10 top-0 right-0 left-0 bottom-0 flex flex-1 flex-col justify-center items-center gap-y-10 px-3 sm:px-6 backdrop-blur-3xl pb-[5%]">
 				<Headline />
